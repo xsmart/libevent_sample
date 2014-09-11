f9c0c4e409edb9677d6ce12d5bca49fa77ba4136
yandongxiao <13837346363@163.com>
2014-09-11 10:24:03 +0800

    pass param to callback functions

--- timer/main.c
+++ timer/main.c
@@ -1,42 +1,42 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <event.h>
 #include <unistd.h>
 
+typedef struct {
+	int num;
+	int maxnum;
+}cb_param_t;
+
 void say_hello(evutil_socket_t fd, short what, void *arg) 
 {
-       const char *data = arg;
-        printf("%s %d:%s%s%s%s\n",
-		data,
-            (int) fd,
+       cb_param_t *param = (cb_param_t*)arg;
+        printf("%s%s%s%s\n",
             (what&EV_TIMEOUT) ? " timeout" : "",
             (what&EV_READ)    ? " read" : "",
             (what&EV_WRITE)   ? " write" : "",
             (what&EV_SIGNAL)  ? " signal" : "");
+       printf("num==%d, maxnum=%d\n", param->num++, param->maxnum);
 }
 
  
 int main(int argc, const char* argv[])
 {
   struct event *ev;
-  struct timeval tv = {3,0};
+  struct timeval tv = {1,0};
   struct event_base *base;
 
   int fd[2];
   pipe(fd);
 
   int pid = 0;
-/*
-  if((pid = fork()) == 0) {
-	close(fd[0]);
-	dup2(fd[1], STDOUT_FILENO);
-	printf("child\n");
-	//write(fd[1], "child", 5);
-  }
-*/
   close(fd[1]);
   base = event_base_new();
-  ev = event_new(base, fd[0], EV_TIMEOUT | EV_READ, say_hello, "get an event on socket");
+  cb_param_t *param = malloc(sizeof(cb_param_t));
+  param->num=0;
+  param->maxnum=100;
+  ev = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, say_hello, param);
   event_add(ev, &tv);
   event_base_dispatch(base);
   return 0;
