948ec1f981c78b20909b97169b70526d8220783f
yandongxiao <13837346363@163.com>
2015-04-11 15:43:31 +0800

    update

--- event/event_priority2.c
+++ event/event_priority2.c
@@ -9,11 +9,14 @@ struct event *ev1;
 struct event *ev2;
 struct event *ev3;
 
-/*strict priority queues. Active events with a lower priority are always processed before events with a higher priority.  */
-/*  By default, Libevent assigns the middle priority to all events unless their priority is explicitly set. */
+/* when to change the pending event to active ? 
+ * After same priority avtive events done, check.
+ */
+
 void
 cb_1(evutil_socket_t fd, short evtype, void *arg)
 {
+    sleep (2);
     printf("cb_1\n");
 }
 
@@ -26,6 +29,7 @@ cb_2(evutil_socket_t fd, short evtype, void *arg)
 void
 cb_3(evutil_socket_t fd, short evtype, void *arg)
 {
+    sleep (2);
     printf("cb_3\n");
 }
 
@@ -33,18 +37,23 @@ int
 main()
 {
     pipe(pp);
-    struct event_base *base = event_base_new();
-    int n = event_base_get_npriorities(base);
-    printf ("default npriorities : <%d>\n", n);
+
+    /* invoke event_config_set_max_dispatch_interval to change the default behavior. */
+    struct event_config *config = event_config_new ();
+    struct timeval interval = {1,0};
+    event_config_set_max_dispatch_interval (config, &interval, 1, 1);
+    struct event_base *base = event_base_new_with_config(config);
     event_base_priority_init(base, 20); 
     
     ev1 = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, cb_1, NULL);
     ev2 = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, cb_2, NULL);
     ev3 = event_new(base, -1, EV_TIMEOUT | EV_PERSIST, cb_3, NULL);
 
+    /* If they are different priority, low priority will have no chance to exec. 
+     * No matter event_config_set_max_dispatch_interval
+     */
     event_priority_set(ev1, 18);    /* 0-19 */
     event_priority_set(ev3, 11);
-
 
     struct timeval tv = {1,0};
     event_add(ev1, &tv);
